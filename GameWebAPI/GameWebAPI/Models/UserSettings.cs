145e3aa9e399ced056210a84eb07cfed747a0610
Johkie <johannes_hp@hotmail.com>
2020-02-06 10:23:50 +0100

    init commit

---
+++ GameWebAPI/GameWebAPI/Models/UserSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameWebAPI.Models
+{
+    public class UserSettings
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ballColor { get; set; }
+    }
+}
