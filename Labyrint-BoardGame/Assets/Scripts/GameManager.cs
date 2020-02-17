9ff773443ce1160e57326902ff2215d8ecd6023b
Johannes Andersson <johannes_hp@hotmail.com>
2020-02-17 18:57:21 +0100

    added ingame menu

--- Labyrint-BoardGame/Assets/Scripts/GameManager.cs
+++ Labyrint-BoardGame/Assets/Scripts/GameManager.cs
@@ -5,29 +5,58 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject MainMenuUI;
+    public GameObject PauseMenuUI;
+    public GameObject InGameUI;
+
+    private bool isGameRunning;
 
     void Awake()
     {
+        isGameRunning = false;
+
         if (MainMenuUI.activeSelf == true)
         {
             Time.timeScale = 0f;
         }
         else
         {
-            Time.timeScale = 1f;
+            StartGame();
         }
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isGameRunning)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                PauseGame();
+            }
+        }
     }
 
     public void StartGame()
     {
+        InGameUI.SetActive(true);
         Time.timeScale = 1f;
+        isGameRunning = true;
+    }
+
+    public void PauseGame()
+    {
+        InGameUI.SetActive(false);
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGameRunning = false;
+    }
+
+    public void ResetGame()
+    {
+        InGameUI.SetActive(false);
+        MainMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGameRunning = false;
     }
 
     public void ExitGame()
